8a797477fb05233afe0336049f065c98d69a8e55
Termset <18351926130@163.com>
2020-09-01 21:26:19 +0800

    多重背包问题

--- MultiKnapsack.cpp
+++ MultiKnapsack.cpp
@@ -14,7 +14,7 @@ int main()
     int dp[11] = { 0 };
     for (int i = 1; i <= 5; i++)
         for (int k = 1; k <= cot[i];k++)
-            for (int j = 10; j >= w[i]; j--)
+            for (int j = total_weight; j >= w[i]; j--)
                 dp[j] = max(dp[j], dp[j - w[i]] + v[i]);
     cout << "总的价值为: " << dp[10] << endl;
     return 0;
